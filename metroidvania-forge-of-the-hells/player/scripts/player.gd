77b5851649b375a0b253d0c22dda6c31ebf5f569
House Wren Engineering <142348312+housewrenengineering@users.noreply.github.com>
2026-03-05 22:08:07 -0500

    ch03ep04 player hud health bar

--- metroidvania-forge-of-the-hells/player/scripts/player.gd
+++ metroidvania-forge-of-the-hells/player/scripts/player.gd
@@ -28,8 +28,15 @@ var previous_state : PlayerState :
 #endregion
 
 #region /// player stats
-var hp : float = 20
-var max_hp : float = 20
+var hp : float = 20 : 
+	set( value ):
+		hp = clampf( value, 0, max_hp )
+		Messages.player_health_changed.emit( hp, max_hp )
+var max_hp : float = 20 :
+	set( value ):
+		max_hp = value
+		Messages.player_health_changed.emit( hp, max_hp )
+		
 var dash : bool = false
 var double_jump : bool = false
 var ground_slam : bool = false
@@ -50,7 +57,19 @@ func _ready() -> void:
 	Messages.player_healed.connect( _on_player_healed )
 	pass
 	
-func _unhandled_input(event: InputEvent) -> void:
+func _unhandled_input( event: InputEvent ) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_MINUS:
+			if Input.is_key_pressed( KEY_SHIFT ):
+				max_hp -= 10
+			else:
+				hp -= 2
+		elif event.keycode == KEY_EQUAL:
+			if Input.is_key_pressed( KEY_SHIFT ):
+				max_hp += 10
+			else:
+				hp += 2
+		
 	if event.is_action_pressed("action"): 
 		Messages.player_interacted.emit( self )
 	change_state( current_state.handle_input( event ))
